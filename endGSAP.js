1019849feeec28fe0e66be8b2ef64013ddbc9a7b
Alek Pialucha <pialuchaa@btinternet.com>
2023-02-02 16:21:15 +0000

    formatting

--- endGSAP.js
+++ endGSAP.js
@@ -1,11 +1,11 @@
-(function(){
-    gsap.to(parentNode, {duration: 0, opacity: 1})
-    let primaryCopy = parentNode.querySelector(".primaryCopy");
-    let secondaryCopy = parentNode.querySelector(".secondaryCopy");
-    let primaryTl = gsap.timeline();
-    let secondaryTl = gsap.timeline();
-    primaryTl.from(primaryCopy, {duration: 1.5, x: "300px", opacity: 0});
-    secondaryTl.from(secondaryCopy, {duration: 1.5, x: "300px", opacity: 0});
-    gsap.to(parentNode, {duration: 0, opacity: 0, delay: 6})
-    console.log("End of Animation 3");
+(function () {
+	gsap.to(parentNode, { duration: 0, opacity: 1 });
+	let primaryCopy = parentNode.querySelector(".primaryCopy");
+	let secondaryCopy = parentNode.querySelector(".secondaryCopy");
+	let primaryTl = gsap.timeline();
+	let secondaryTl = gsap.timeline();
+	primaryTl.from(primaryCopy, { duration: 1.5, x: "300px", opacity: 0 });
+	secondaryTl.from(secondaryCopy, { duration: 1.5, x: "300px", opacity: 0 });
+	gsap.to(parentNode, { duration: 0, opacity: 0, delay: 6 });
+	console.log("End of Animation 3");
 })();
